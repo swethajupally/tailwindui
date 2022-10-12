874b89bfcbd97f4354dbb064ba84cdf157fdd8d7
swethajupally <swethajupally@gmail.com>
2022-10-12 16:12:17 +0530

    init

---
+++ tailwind/src/App.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+import Example from "./Example";
+import Productlist from "./Productlist";
+import Promocode from "./Promocode";
+
+
+
+function App() {
+  return (
+    <div>
+      <Example/>
+      <Productlist/>
+      <Promocode/>
+    </div>
+   
+    
+  );
+}
+
+export default App;
